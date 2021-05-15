357a8c1f7dd7ee29da165bea84a02cd6ec3b5943
Nardin Lachowski <nardin@live.com>
2021-05-15 15:23:10 -0400

    added remaining icons to header with new styling

--- components/Header.js
+++ components/Header.js
@@ -16,6 +16,11 @@ export default function Header() {
         <header className=''>
             <div>
                 <HeaderItem title="HOME" Icon={HomeIcon} />
+                <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+                <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+                <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+                <HeaderItem title="SEARCH" Icon={SearchIcon} />
+                <HeaderItem title="ACCOUNT" Icon={UserIcon} />
             </div>
             
             <Image className="object-contain" src="https://links.papareact.com/ua6" width={200} height={100} />
