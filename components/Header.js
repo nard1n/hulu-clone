3c0a09b686cc8a48739d1e7a0a6a4c39b9280e5c
Nardin Lachowski <nardin@live.com>
2021-05-15 14:45:24 -0400

    added necessary components

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+export default function Header() {
+    return (
+        <header className=''>
+            
+            <Image className="object-contain" src="https://links.papareact.com/ua6" width={200} height={100} />
+
+        </header>
+    )
+}
+
