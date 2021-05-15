0f5dbe4d6575322def4e99257ad35c16d509c015
Nardin Lachowski <nardin@live.com>
2021-05-15 17:22:15 -0400

    set up api call to tmdb and env variables

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,11 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
 import Navigation from '../components/Navigation'
-// import Results from '../components/Results'
+import Results from '../components/Results'
+import requests from '../utils/requests'
 
-export default function Home() {
+export default function Home({results}) {
+  
   return (
     <div >
       <Head>
@@ -20,8 +22,23 @@ export default function Home() {
       <Navigation />
 
       {/* Results/Main */}
-      {/* <Results /> */}
+      <Results results={results} />
 
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+      }`
+    ).then(res => res.json());
+  return{
+    props: {
+      results: request.results,
+    }
+}
 }
