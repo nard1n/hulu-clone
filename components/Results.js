83989070ebcf22bbc4e9f170b3559a6974f1ca62
Nardin Lachowski <nardin@live.com>
2021-05-15 20:53:39 -0400

    fixed rendering of css file for tailwind

--- components/Results.js
+++ components/Results.js
@@ -1,7 +1,11 @@
-export default function Results() {
+import Thumbnail from "./Thumbnail";
+
+export default function Results({ results }) {
     return (
         <div>
-            
+            {results.map((result) => (
+                <Thumbnail key={result.id} result={result} />
+            ))}
         </div>
-    )
+    );
 }
